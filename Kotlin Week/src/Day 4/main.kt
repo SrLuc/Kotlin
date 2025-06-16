f1fee30205526a947462e638eb21d8096339e50c
ruanrlr <ruanlucenaramos@gmail.com>
2025-06-16 12:17:17 -0300

    Studying throw execeptions

---
+++ Kotlin Week/src/Day 4/main.kt
@@ -0,0 +1,25 @@
+fun main(){
+
+    //Tratamentos de Erros com Throw
+
+    fun divider(a:Int, b:Int):Int{
+        if (a == 0 || b == 0) {
+            throw IllegalArgumentException("\nDivision by zero not supported")
+        }
+        return a / b
+    }
+
+
+    //Criando os própios tratamento de erros
+    class meuErroCustomizado(msg:String):Exception(msg)
+    fun dividerAgain(a:Int, b:Int):Int{
+        if (a == 0 || b == 0){
+            throw meuErroCustomizado("\nDivision by zero not supported")
+        }
+        return a / b
+    }
+
+    divider(2,1)
+    divider(3,0)
+}
+
