f524c3e450b2a0b7c6064e839b00e3b844eb31e0
ruanrlr <ruanlucenaramos@gmail.com>
2025-06-25 16:57:42 -0300

    Adding JAVA classes with POO

--- Kotlin Week/src/Day 4/main.kt
+++ Kotlin Week/src/Day 4/main.kt
@@ -1,25 +1,25 @@
-fun main(){
+    fun main(){
 
-    //Tratamentos de Erros com Throw
+        //Tratamentos de Erros com Throw
 
-    fun divider(a:Int, b:Int):Int{
-        if (a == 0 || b == 0) {
-            throw IllegalArgumentException("\nDivision by zero not supported")
+        fun divider(a:Int, b:Int):Int{
+            if (a == 0 || b == 0) {
+                throw IllegalArgumentException("\nDivision by zero not supported")
+            }
+            return a / b
         }
-        return a / b
-    }
 
 
-    //Criando os própios tratamento de erros
-    class meuErroCustomizado(msg:String):Exception(msg)
-    fun dividerAgain(a:Int, b:Int):Int{
-        if (a == 0 || b == 0){
-            throw meuErroCustomizado("\nDivision by zero not supported")
+        //Criando os própios tratamento de erros
+        class meuErroCustomizado(msg:String):Exception(msg)
+        fun dividerAgain(a:Int, b:Int):Int{
+            if (a == 0 || b == 0){
+                throw meuErroCustomizado("\nDivision by zero not supported")
+            }
+            return a / b
         }
-        return a / b
-    }
 
-    divider(2,1)
-    divider(3,0)
-}
+        divider(2,1)
+        divider(3,0)
+    }
 
